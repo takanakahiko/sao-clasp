977d11e871b2e241a138a1adece5c2e989ee300d
godan <matsushita@godan.xyz>
2019-07-27 13:48:11 +0900

    Fix rewrite test file

--- test/test.js
+++ test/test.js
@@ -2,10 +2,9 @@ import path from 'path'
 import test from 'ava'
 import sao from 'sao'
 
-const template = path.join(__dirname, '..')
+const generator = path.join(__dirname, '..')
 
 test('defaults', async t => {
-  const stream = await sao.mockPrompt(template)
-
-  t.snapshot(stream.fileList, 'Generated files')
+  const stream = await sao.mock({ generator })
+  t.true(stream.fileList.includes('src/index.ts'))
 })
